4d6d26ab8267af59df7366aa33cc7d262dee1ad3
Ansari <ping@ansari.wtf>
2026-02-25 12:44:31 +0530

    Viewport for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,46 +1,60 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-	 		require("telescope.actions")
-			require('telescope').setup({
-			pickers = {
-					find_files = {
-						find_command = { "rg", "--files",
-							"--hidden","--follow",
-							"--glob", "!.git/*",
-							"--glob", "!node_modules/*"
-							}
+			require("telescope.actions")
+			require("telescope").setup({
+
+				defaults = {
+					layout_strategy = "horizontal",
+					layout_config = {
+						horizontal = {
+							preview_width = 0.6,
 						},
+					},
+				},
+
+				pickers = {
+					find_files = {
+						find_command = {
+							"rg",
+							"--files",
+							"--hidden",
+							"--follow",
+							"--glob",
+							"!.git/*",
+							"--glob",
+							"!node_modules/*",
+						},
+					},
 					live_grep = {
 						additional_args = function()
 							return { "--hidden", "--glob", "!.git/*" }
-							end,
-						}
-					}
+						end,
+					},
+				},
 			})
-		end
+		end,
 	},
 	{
-    'mrloop/telescope-git-branch.nvim',
+		"mrloop/telescope-git-branch.nvim",
 		config = function()
-			require('telescope').load_extension('git_branch')
-		end
+			require("telescope").load_extension("git_branch")
+		end,
 	},
 	{
-		'nvim-telescope/telescope-ui-select.nvim',
+		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
-			require("telescope").setup {
+			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown {
-						}
-
-					}
-				}
-			}
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 			require("telescope").load_extension("ui-select")
-		end
-	}
+		end,
+	},
 }
