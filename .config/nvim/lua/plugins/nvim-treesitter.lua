a7ad98795aea412d8a1f502ef0494dcff9b37d29
Ansari <ping@ansari.wtf>
2026-05-12 16:31:03 +0530

    nvim-treesitter archived so introducing treesittermanager

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,30 @@
+-- return {
+-- 	"nvim-treesitter/nvim-treesitter",
+-- 	build = ":TSUpdate",
+-- 	config = function()
+-- 		local configs = require("nvim-treesitter.configs")
+-- 		configs.setup({
+-- 			ensure_installed = { "c", "cpp", "go",
+-- 				"rust", "css", "lua", "vim",
+-- 				"vimdoc", "javascript", "html",
+-- 				"typescript", "json", "make", "cmake",
+-- 				"python", "yaml", "bash" },
+-- 			indent = { enable = true }
+-- 		})
+-- 	end
+-- }
+
+
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
+	"romus204/tree-sitter-manager.nvim",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			ensure_installed = { "c","cpp","go",
-				"rust","css", "lua", "vim",
-				"vimdoc", "javascript", "html",
-				"typescript","json","make","cmake",
-				"python","yaml","bash"},
-			highlight = { enable = true },
-			indent = { enable = true }
+		require("tree-sitter-manager").setup({
+			ensure_installed = {
+				"c", "cpp", "go", "rust", "css", "lua",
+				"javascript", "typescript", "json",
+				"python", "bash", "markdown", "markdown_inline",
+			},
+			auto_install = true,
 		})
 	end
 }
