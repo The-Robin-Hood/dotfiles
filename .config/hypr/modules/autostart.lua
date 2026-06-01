7a17c8bbe4b8f5def3335eae87af5174326c677a
Ansari <ping@ansari.wtf>
2026-06-01 13:56:12 +0530

    autostart update

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -7,9 +7,12 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("swaync")
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("awww-daemon")
-	hl.dsp.focus({ workspace = "Code" })
 
 	-- autostart applications
 	hl.exec_cmd("$(awk -F= '/^Exec/ {print $2; exit}' ~/.local/share/applications/Beeper.desktop)")
 	hl.exec_cmd("thunderbird")
+
+	hl.timer(function()
+		hl.dispatch(hl.dsp.focus({workspace ="5"}))
+	end,{timeout= 1500,type="oneshot"})
 end)
--- .config/hypr/modules/misc.lua
+++ .config/hypr/modules/misc.lua
@@ -4,6 +4,5 @@ hl.config({
 		disable_splash_rendering = true,
 		mouse_move_enables_dpms = true,
 		allow_session_lock_restore = true,
-		initial_workspace_tracking = 1,
 	},
 })
