f61215f500362c1e4b5390f268bfbf87a64dc831
Ansari <ping@ansari.wtf>
2026-08-05 20:43:06 +0530

    plugin enable

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -9,6 +9,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("swaync")
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("awww-daemon")
+	hl.exec_cmd("hyprpm reload")
 
 	hl.exec_cmd(vars.sshAgentRestart)
 
